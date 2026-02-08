02ef7ee5704aeb025b87b9297ab8ddd88b91f1b7
Shubham Shelke <shubhamshelke6103@gmail.com>
2026-02-08 12:31:11 +0530

    change in mongoDb url

--- src/config/env.js
+++ src/config/env.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 module.exports = {
   port: process.env.PORT || 3000,
   // Use IPv4 loopback to avoid IPv6 (::1) connection issues on some Windows setups
-  mongodbUri: process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/porttivo',
+  mongodbUri: process.env.MONGODB_URI || 'mongodb+srv://ShubhamShelke:Shubhamshelke@cluster0.23riiuz.mongodb.net/porttivo?appName=Cluster0',
   jwtSecret: process.env.JWT_SECRET || 'default-secret-key',
   jwtExpiresIn: process.env.JWT_EXPIRES_IN || '7d',
   jwtRefreshExpiresIn: process.env.JWT_REFRESH_EXPIRES_IN || '30d',
