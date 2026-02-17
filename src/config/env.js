7282f27167ce525bbf06aea39db7a770bba754c0
Shubham Shelke <shubhamshelke6103@gmail.com>
2026-02-17 11:42:04 +0530

    Add new mongo

--- src/config/env.js
+++ src/config/env.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 module.exports = {
   port: process.env.PORT || 3000,
-  mongodbUri: process.env.MONGODB_URI || 'mongodb+srv://shubhamshelke6103_db:ShubhamShelke011@cluster0.23riiuz.mongodb.net/porttivo?appName=Cluster0',
+  mongodbUri: process.env.MONGODB_URI || 'mongodb+srv://shubhamshelke6103_db:shubhamshelke@cluster0.23riiuz.mongodb.net/porttivo?appName=Cluster0',
   jwtSecret: process.env.JWT_SECRET || 'default-secret-key',
   jwtExpiresIn: process.env.JWT_EXPIRES_IN || '7d',
   jwtRefreshExpiresIn: process.env.JWT_REFRESH_EXPIRES_IN || '30d',
